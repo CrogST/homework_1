bb6ce7981f4f4dbec946e7e5d083e08e7e2964da
CrogST <50702589+CrogST@users.noreply.github.com>
2019-10-02 10:24:54 +0300

    Add files

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "lib.h"
+
+#include <iostream>
+//#include <spdlog/spdlog.h>
+
+int main()
+{
+    //auto logger = spdlog::stdout_logger_mt("console");
+    //logger->info("version {} was started", version());
+
+    std::cout << "build " << version() << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
